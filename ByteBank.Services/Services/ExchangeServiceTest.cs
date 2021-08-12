b3eda308dc57847e160baf26aad0ac77a62a92b9
= <=>
2021-08-12 15:18:26 -0300

    Implementing IExchangeService interface.

---
+++ ByteBank.Services/Services/ExchangeServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using ByteBank.Services.Services.Intefaces;
+using System;
+using System.Threading.Tasks;
+
+namespace ByteBank.Services.Services
+{
+    public class ExchangeServiceTest : IExchangeService
+    {
+        private readonly Random _rdm = new Random();
+        public async Task<decimal> Calculate(string sourceCurrency, string destCurrency, decimal value)
+        {
+            return await Task.Run(() =>
+            {
+                return value * (decimal)_rdm.NextDouble();
+            });
+        }
+    }
+}
